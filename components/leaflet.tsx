d9aa5048ae6f8668e8928e165948629b5c8b704b
charlohc <Charlottee.Christiansen@gmail.com>
2023-12-07 18:17:02 +0100

    processing data to display average price on map

--- components/leaflet.tsx
+++ components/leaflet.tsx
@@ -1,21 +1,89 @@
-"use client";
+ "use client";
+import React, {useEffect, useState} from 'react';
+import Papa from 'papaparse';
 import "leaflet/dist/leaflet.css";
-import { MapContainer, Marker, Popup, TileLayer, GeoJSON } from "react-leaflet";
-import "leaflet/dist/leaflet.css";
+import { MapContainer, TileLayer, GeoJSON } from "react-leaflet";
 import districts from "@/data/madrid-districts.json";
+import dataMap from "@/data/average_prices.csv"; // Import CSV file
 import { GeoJsonObject } from "geojson";
 
+interface CsvEntry {
+  month: number;
+  price: number;
+  region: string;
+}
 interface LeafletMapProps {
   center: L.LatLngExpression;
   zoom: number;
 }
 
 const Leaflet: React.FC<LeafletMapProps> = ({ center, zoom }) => {
-  const showDistrictName = (feature: any, layer: L.Layer) => {
-    if (feature.properties && feature.properties.name) {
-      layer.bindPopup(feature.properties.name);
-      layer.bindTooltip(feature.properties.name, {permanent: true, direction: "center", className: "suburb-label"});
+  const [csvData, setCsvData] = useState<CsvEntry[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        // Use the imported CSV data
+        const csvString = dataMap; // Assuming dataMap is a string
+        console.log("CSV string:", dataMap);
+
+        // Create a Blob from the CSV string
+        const csvBlob = new Blob([csvString], { type: 'text/csv' });
+
+        for (let i = 0; i < csvString.length; i++) {
+          console.log(csvString.at(i));
+        }
+
+      } catch (error) {
+        console.error("Error fetching or parsing CSV data:", error);
+      }
+    };
+
+  // Call the fetchData function
+  fetchData();
+}, []); // Run the effect only once on mount
+
+  const calculateAveragePrice = (month: number, region: string) => {
+    console.log("moth: " + month + "region " + region);
+    // Filter data for the specified month and region
+    const filteredData = csvData.filter(
+      (entry) => entry.month === month && entry.region === region
+    );
+
+    // Check if there is data for the specified month and region
+    if (filteredData.length > 0) {
+      // Calculate average price
+      const averagePrice =
+        filteredData.reduce((sum, entry) => sum + entry.price, 0) /
+        filteredData.length;
+
+      return averagePrice.toFixed(2); // Format to two decimal places
+    } else {
+      return "No data"; // Handle the case where there is no data for the specified month and region
     }
+  };
+
+  const showDistrictName = (feature: any, layer: L.Layer) => {
+    const region = feature.properties.name;
+
+    // Create a tooltip
+    const tooltipContent = `<strong>${region}</strong>`;
+
+    // Bind tooltip to the layer
+    layer.bindTooltip(tooltipContent, { permanent: true, direction: "center", className: "zone-tooltip" });
+
+    layer.on({
+      click: () => {
+        const month = 1; // Set to the desired month
+
+        // Calculate average price
+        const averagePrice = calculateAveragePrice(month, region);
+
+        // Display in the popup
+        layer.bindPopup(`Region: ${region}<br/>Average Price: $${averagePrice}`);
+        layer.openPopup();
+      },
+    });
   };
 
   return (
--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+module.exports = {
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.(csv)$/,
+      use: {
+        loader: 'csv-loader',
+        options: {
+          dynamicTyping: true,
+          header: true,
+          skipEmptyLines: true,
+        },
+      },
+    });
+    return config;
+  },
+};
