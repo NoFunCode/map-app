e2568a88fd372f02283362194962d6033bb88e44
charlohc <Charlottee.Christiansen@gmail.com>
2023-12-08 22:18:59 +0100

    can display all prices map and display color depending on price

--- components/leaflet.tsx
+++ components/leaflet.tsx
@@ -1,13 +1,11 @@
 "use client";
 import React, { useEffect, useState } from 'react';
-import Papa from 'papaparse';
+import L from 'leaflet';
 import "leaflet/dist/leaflet.css";
 import { MapContainer, TileLayer, GeoJSON } from "react-leaflet";
 import districts from "@/data/madrid-districts.json";
 import { GeoJsonObject } from "geojson";
 import stringSimilarity from 'string-similarity';
-import csvParser from "csv-parser";
-import entry from "next/dist/server/typescript/rules/entry";
 
 interface CsvEntry {
   month: number;
@@ -20,86 +18,102 @@ interface LeafletMapProps {
   zoom: number;
 }
 
-
-let jsonData: CsvEntry[] = []; // Declare jsonData as a global variable
+let jsonData: CsvEntry[] = [];
 
 const Leaflet: React.FC<LeafletMapProps> = ({ center, zoom }) => {
-  const [csvData, setCsvData] = useState<CsvEntry[]>([]);
   const [entries, setEntries] = useState<CsvEntry[]>([]);
+    const [csvData, setCsvData] = useState<CsvEntry[]>([]);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        // Import CSV data directly
         // eslint-disable-next-line @next/next/no-assign-module-variable
         const module = await import('@/data/average_prices.csv');
-        // Convert CSV data to Blob
         const jsonString = JSON.stringify(module.default);
-        jsonData = JSON.parse(jsonString); // Update the global jsonData variable
+        jsonData = JSON.parse(jsonString);
 
-        // Log the JSON data to verify it
-       // console.log(jsonData);
-
-        // Set the parsed JSON data to the state
         setCsvData(jsonData);
 
         jsonData.forEach((entry: CsvEntry) => {
+          const bestMatch = stringSimilarity.findBestMatch(entry.region.trim(), districts.features.map((feature: any) => feature.properties.name.trim()));
+          entry.region = districts.features[bestMatch.bestMatchIndex].properties.name.trim();
+        });
+
+        jsonData.forEach((entry: CsvEntry) => {
           if (entry.month === 1) {
-            //console.log(`Region: ${entry.region}, Month: ${entry.month} Price: ${entry.price}`);
             setEntries(prevEntries => [...prevEntries, entry]);
           }
         });
       } catch (e) {
-        console.log("error", e);
+        console.error("Error fetching data:", e);
       }
     };
 
-    // Call the fetchData function
     fetchData();
-  }, []); // Run the effect only once on mount
+  }, []);
 
-useEffect(() => {
-    // Log the global jsonData whenever it changes
-    //console.log("jsonData globally accessible: ", jsonData);
-  }, [jsonData]); // Run this effect whenever jsonData changes
 
-const showDistrictName = (feature: any, layer: L.Layer) => {
-  //this is before -- it needs to be after its populated
-   //console.log("jsonData globally accessible: ", jsonData);
-  const nameFromMap = feature.properties.name;
-  //console.log("name from map " + nameFromMap);
-  //console.log("region " + region + " month " + month + " price " + price);
+  const showDistrictName = (feature: any, layer: L.Layer) => {
+    const nameFromMap = feature.properties.name.trim();
+    const tooltipContent = `<strong>${nameFromMap}</strong>`;
 
-  // Create a tooltip with the region name
-  const tooltipContent = `<strong>${nameFromMap}</strong>`;
+    if (layer) {
+      layer.bindTooltip(tooltipContent, { permanent: true, direction: "center", className: "zone-tooltip" });
 
-  // Bind tooltip to the layer if it exists
-  if (layer) {
-    layer.bindTooltip(tooltipContent, { permanent: true, direction: "center", className: "zone-tooltip" });
+      layer.on({
+        click: () => handleDistrictClick(nameFromMap, layer),
+      });
+    }
+  };
 
-    layer.on({
-      click: () => {
-        const month = 1; // Set to the desired month
-        const clickedDistrict = nameFromMap;
+  const handleDistrictClick = (clickedDistrict: string, layer: L.Layer) => {
+    const month = 1;
+    const bestMatch = stringSimilarity.findBestMatch(clickedDistrict, jsonData.map(entry => entry.region.trim()));
+    const matchedEntry = jsonData[bestMatch.bestMatchIndex];
 
-        // Find the entry for the clicked district and specified month with string similarity
-        const bestMatch = stringSimilarity.findBestMatch(clickedDistrict, jsonData.map(entry => entry.region));
-        const matchedEntry = jsonData[bestMatch.bestMatchIndex];
-
-        if (bestMatch.bestMatch.rating > 0.5) {
-          // Display the price information in a popup
-          const popupContent = `Region: ${clickedDistrict}<br/>Month: ${month}<br/>Price: ${matchedEntry.price}`;
-          layer.bindPopup(popupContent);
-          layer.openPopup();
-        } else {
-          // No close match found
-          layer.bindPopup(`Region: ${clickedDistrict}<br/>No data for Month ${month}`);
-          layer.openPopup();
+    if (bestMatch.bestMatch.rating > 0.5) {
+      jsonData.forEach((entry, index) => {
+        if (index === bestMatch.bestMatchIndex) {
+          entry.region = clickedDistrict;
         }
-      },
-    });
-  }
-};
+      });
+
+      const popupContent = `Region: ${clickedDistrict}<br/>Month: ${month}<br/>Price: ${matchedEntry.price}`;
+      layer.bindPopup(popupContent);
+      layer.openPopup();
+    } else {
+      layer.bindPopup(`Region: ${clickedDistrict}<br/>No data for Month ${month}`);
+      layer.openPopup();
+    }
+  };
+
+  const style = (feature: any) => {
+    const districtName = feature.properties.name.trim();
+    const entry = jsonData.find((item) => item.region.trim() === districtName);
+
+    const color = entry ? getColor(entry.price) : 'lightgray';
+
+    return {
+      fillColor: color,
+      weight: 2,
+      opacity: 1,
+      color: 'white',
+      dashArray: '3',
+      fillOpacity: 0.7,
+    };
+  };
+
+  const getColor = (price: number) => {
+    return price > 1000
+      ? '#063578'
+      : price > 500
+      ? '#0f4973'
+      : price > 100
+      ? '#306586'
+      : price > 50
+      ? '#66b5e5'
+      : '#9ecae1';
+  };
 
   return (
     <MapContainer
@@ -114,6 +128,7 @@ const showDistrictName = (feature: any, layer: L.Layer) => {
       />
       <GeoJSON
         data={districts as GeoJsonObject}
+        style={style}
         onEachFeature={showDistrictName}
       />
     </MapContainer>
