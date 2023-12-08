5818c0e19bd06180c3c05e3ebe3449eecd9f0fd7
charlohc <Charlottee.Christiansen@gmail.com>
2023-12-08 13:29:15 +0100

    made improvements

--- components/leaflet.tsx
+++ components/leaflet.tsx
@@ -5,6 +5,8 @@ import "leaflet/dist/leaflet.css";
 import { MapContainer, TileLayer, GeoJSON } from "react-leaflet";
 import districts from "@/data/madrid-districts.json";
 import { GeoJsonObject } from "geojson";
+import csvParser from "csv-parser";
+import entry from "next/dist/server/typescript/rules/entry";
 
 interface CsvEntry {
   month: number;
@@ -17,79 +19,86 @@ interface LeafletMapProps {
   zoom: number;
 }
 
+
+let jsonData: CsvEntry[] = []; // Declare jsonData as a global variable
+
 const Leaflet: React.FC<LeafletMapProps> = ({ center, zoom }) => {
   const [csvData, setCsvData] = useState<CsvEntry[]>([]);
+  const [entries, setEntries] = useState<CsvEntry[]>([]);
 
   useEffect(() => {
     const fetchData = async () => {
-       try {
-    // Import CSV data directly
-         // eslint-disable-next-line @next/next/no-assign-module-variable
-    const module = await import('@/data/average_prices.csv');
-    // Convert CSV data to Blob
-    const jsonString = JSON.stringify(module.default);
-    const jsonData = JSON.parse(jsonString);
+      try {
+        // Import CSV data directly
+        // eslint-disable-next-line @next/next/no-assign-module-variable
+        const module = await import('@/data/average_prices.csv');
+        // Convert CSV data to Blob
+        const jsonString = JSON.stringify(module.default);
+        jsonData = JSON.parse(jsonString); // Update the global jsonData variable
 
-    // Log the JSON data to verify it
-    console.log(jsonData);
+        // Log the JSON data to verify it
+        console.log(jsonData);
 
-    // Set the parsed JSON data to the state
-    setCsvData(jsonData);
+        // Set the parsed JSON data to the state
+        setCsvData(jsonData);
 
-     jsonData.forEach((entry: CsvEntry) => {
-       if (entry.month === 1) {
-         console.log(`Region: ${entry.region}, Price: ${entry.price}`);
-       }})
-  } catch (e) {
-    console.log("error", e);
-  }
+        jsonData.forEach((entry: CsvEntry) => {
+          if (entry.month === 1) {
+            console.log(`Region: ${entry.region}, Month: ${entry.month} Price: ${entry.price}`);
+            setEntries(prevEntries => [...prevEntries, entry]);
+          }
+        });
+      } catch (e) {
+        console.log("error", e);
+      }
     };
+
     // Call the fetchData function
     fetchData();
   }, []); // Run the effect only once on mount
 
-  const calculateAveragePrice = (month: number, region: string) => {
-    console.log("moth: " + month + "region " + region);
-    // Filter data for the specified month and region
-    const filteredData = csvData.filter(
-      (entry) => entry.month === month && entry.region === region
-    );
+useEffect(() => {
+    // Log the global jsonData whenever it changes
+    //console.log("jsonData globally accessible: ", jsonData);
+  }, [jsonData]); // Run this effect whenever jsonData changes
 
-    // Check if there is data for the specified month and region
-    if (filteredData.length > 0) {
-      // Calculate average price
-      const averagePrice =
-        filteredData.reduce((sum, entry) => sum + entry.price, 0) /
-        filteredData.length;
+const showDistrictName = (feature: any, layer: L.Layer) => {
+  //this is before -- it needs to be after its populated
+   console.log("jsonData globally accessible: ", jsonData);
+  const nameFromMap = feature.properties.name;
+  console.log("name from map " + nameFromMap);
+  //console.log("region " + region + " month " + month + " price " + price);
 
-      return averagePrice.toFixed(2); // Format to two decimal places
-    } else {
-      return "No data"; // Handle the case where there is no data for the specified month and region
-    }
-  };
+  // Create a tooltip with the region name
+  const tooltipContent = `<strong>${nameFromMap}</strong>`;
 
-  const showDistrictName = (feature: any, layer: L.Layer) => {
-    const region = feature.properties.name;
-
-    // Create a tooltip
-    const tooltipContent = `<strong>${region}</strong>`;
-
-    // Bind tooltip to the layer
+  // Bind tooltip to the layer if it exists
+  if (layer) {
     layer.bindTooltip(tooltipContent, { permanent: true, direction: "center", className: "zone-tooltip" });
 
     layer.on({
       click: () => {
         const month = 1; // Set to the desired month
 
-        // Calculate average price
-        const averagePrice = calculateAveragePrice(month, region);
+        // Filter data for the specified month and region
+        const filteredData = csvData.filter((entry) => entry.month === month && entry.region === nameFromMap);
+        console.log(filteredData.length);
+        console.log("csv data: " + csvData);
 
-        // Display in the popup
-        layer.bindPopup(`Region: ${region}<br/>Average Price: $${averagePrice}`);
-        layer.openPopup();
+        if (filteredData.length > 0) {
+          // Display the price for the specified month
+          const monthPrice = filteredData[0].price;
+          layer.bindPopup(`Region: ${nameFromMap}<br/>Price (Month ${month}): $${monthPrice}`);
+          layer.openPopup();
+        } else {
+          // No data for the specified month and region
+          layer.bindPopup(`Region: ${nameFromMap}<br/>No data for Month ${month}`);
+          layer.openPopup();
+        }
       },
     });
-  };
+  }
+};
 
   return (
     <MapContainer
